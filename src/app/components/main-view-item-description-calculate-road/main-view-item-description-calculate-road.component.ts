ff83329dbc53b6fb09b16313d1f1d16ca6b5dcca
TheEnter3 <wkoziol3ib@gmail.com>
2020-05-25 13:06:45 +0200

    added map

--- src/app/components/main-view-item-description-calculate-road/main-view-item-description-calculate-road.component.ts
+++ src/app/components/main-view-item-description-calculate-road/main-view-item-description-calculate-road.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import LocationPicker from 'location-picker';
 
 @Component({
   selector: 'main-view-item-description-calculate-road',
@@ -6,10 +7,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./main-view-item-description-calculate-road.component.css']
 })
 export class MainViewItemDescriptionCalculateRoadComponent implements OnInit {
+//AIzaSyDpJtDD4m0UXnkt-ndbnjw5w9PzRRccFDw
+  lp: LocationPicker;
 
-  constructor() { }
+  ngOnInit(){
+    this.lp = new LocationPicker('map');
+  }
 
-  ngOnInit(): void {
+  setLocation() {
+    console.log(this.lp);
   }
 
 }
